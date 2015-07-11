70de1d8cc4fb34cfc5bef4fbb27a0206342e8eb9
Ricardo Villanueva <villanueva.ricardo@gmail.com>
2015-07-11 22:34:44 +0100

    update app cms url

--- app/scripts/controllers/blog.js
+++ app/scripts/controllers/blog.js
@@ -9,7 +9,7 @@
  */
 angular.module('ricardoJsApp')
   .controller('BlogCtrl', function ($scope, $http) {
-      $http.get('http://ricardovz.com/wp-json/posts').success(function(data) {
+      $http.get('http://cms.ricardovz.com/wp-json/posts').success(function(data) {
         $scope.posts = data;
       });
   });
