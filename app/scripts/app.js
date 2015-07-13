bf2db26ffb7120f3598465b32286dec38f3d8bcc
Ricardo Villanueva <villanueva.ricardo@gmail.com>
2015-07-13 20:15:53 +0100

    Addind local storage support

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,24 +10,29 @@
  */
 angular
   .module('ricardoJsApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
+      'ngAnimate',
+      'ngCookies',
+      'ngResource',
+      'ngRoute',
+      'ngSanitize',
+      'ngTouch',
+      'LocalStorageModule'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/blog', {
-        templateUrl: 'views/blog.html',
-        controller: 'BlogCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+    .config(['localStorageServiceProvider', function(localStorageServiceProvider){
+      localStorageServiceProvider.setPrefix('ls');
+    }])
+
+    .config(function ($routeProvider) {
+      $routeProvider
+        .when('/', {
+          templateUrl: 'views/main.html',
+          controller: 'MainCtrl'
+        })
+          .when('/blog', {
+          templateUrl: 'views/blog.html',
+          controller: 'BlogCtrl'
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,13 +8,21 @@
  * Controller of the ricardoJsApp
  */
 angular.module('ricardoJsApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $scope.showThanks = false;
 
-    $scope.subscribe = function() {
+    .controller('MainCtrl', function ($scope, $http, localStorageService) {
 
-      $scope.showThanks = true;
+      $scope.showThanks = false;
+      $scope.subscribed = localStorageService.get('subscribed') || false;
 
-      $http.post('http://api.ricardovz.com/subscribe', $scope.email);
-    };
-  });
+
+      /**
+       * Subscribe the email provided for future notifications
+       */
+      $scope.subscribe = function() {
+
+        $scope.showThanks = true;
+        $scope.subscribed = localStorageService.set('subscribed', true);
+
+        $http.post('http://api.ricardovz.com/subscribe', $scope.email);
+      };
+    });
